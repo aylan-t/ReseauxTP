9ed05474290b30e92be09b27425f51569f163568
fwicotex <123177060+fwicotex@users.noreply.github.com>
2025-01-14 21:26:19 -0500

    ClientTP fini (je pense)

--- src/ClientTP.java
+++ src/ClientTP.java
@@ -1,58 +1,100 @@
-import org.w3c.dom.ls.LSOutput;
-
+import javax.naming.AuthenticationException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class ClientTP {
     private static Socket socket;
     private static String adresseIP;
     private static int port;
 
-    public static void main(String[] args) throws Exception {
-        // Adresse et port du serveur
-        receuillirInfos();
-        socket = new Socket(adresseIP, port);
-        System.out.format("Serveur lancé sur [%s:%d]", adresseIP, port);
-
-        DataInputStream in = new DataInputStream(socket.getInputStream());
-        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-    }
-    public static void receuillirInfos() throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        adresseIP = "";
-        port = 0;
-        System.out.println("Veuillez saisir l'addresse IP de votre client");
-        try {
-            adresseIP = sc.nextLine();
-            if (!verifierIP(adresseIP)) {
-                throw new Exception(e);
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                receuillirInfos();
+                validInput = true;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Veuillez réessayer.\n");
             }
-        } catch (Exception e) {
-            System.out.println(e);
         }
 
-        System.out.println("Veuillez saisir le port de votre client");
         try {
-            port = sc.nextInt();
-            if (port != 5000 && port != 5050) {
-                throw new Exception(e);
+            socket = new Socket(adresseIP, port);
+            System.out.format("Connecté au serveur sur [%s:%d]\n", adresseIP, port);
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            System.out.println("Veuillez entrer votre nom d'utilisateur:");
+            String nomUtilisateur = sc.nextLine();
+            System.out.println("Veuillez entrer votre mot de passe:");
+            String mdp = sc.nextLine();
+
+            out.writeUTF(nomUtilisateur);
+            out.writeUTF(mdp);
+
+            String reponseAuth = in.readUTF();
+            if (!verifierReponse(reponseAuth)) {
+                throw new AuthenticationException();
             }
-        } catch (Exception e) {
-            System.out.println(e);
+            else {
+                String historiqueMessages = in.readUTF();
+                System.out.println(historiqueMessages);
+                boolean msgValide;
+                do {
+                    System.out.println("Veuillez écrire un message (Limite de 200 charactères)");
+                    String message = sc.nextLine();
+                    msgValide = validerMessage(message);
+                } while (!msgValide);
+            }
+
+            socket.close();
+        } catch (IOException e) {
+            System.out.println("Erreur lors de la connexion au serveur: " + e.getMessage());
+        } catch (AuthenticationException e) {
+            System.out.println("Erreur dans la saisie du mot de passe");
         }
     }
-    public static boolean verifierIP(String adresseIP) {
-        if (adresseIP == null) {
-            return false;
+
+    public static void receuillirInfos() throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Veuillez saisir l'adresse IP de votre serveur:");
+        adresseIP = sc.nextLine();
+        try {
+            verifierIP(adresseIP);
+        } catch (UnknownHostException e) {
+            throw new Exception("Adresse IP invalide.");
         }
-        String regexIP = "^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\\." +
-                "(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\\." +
-                "(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\\." +
-                "(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$";
-        return Pattern.compile(regexIP).matcher(adresseIP).matches();
+
+        System.out.println("Veuillez saisir le port de votre serveur (5000 ou 5050):");
+        try {
+            port = Integer.parseInt(sc.nextLine());
+            if (port != 5000 && port != 5050) {
+                throw new Exception("Port invalide. Utilisez 5000 ou 5050.");
+            }
+        } catch (NumberFormatException e) {
+            throw new Exception("Le port doit être un nombre entier.");
+        }
+    }
+
+    public static void verifierIP(String adresseIP) throws UnknownHostException {
+        InetAddress.getByName(adresseIP);
+    }
+
+    public static boolean verifierReponse(String reponseAuth) {
+        return reponseAuth.toLowerCase().contains("erreur mdp");
+    }
+
+    public static boolean validerMessage(String message) {
+        return (message != null) && (message.length() < 200);
     }
 }
